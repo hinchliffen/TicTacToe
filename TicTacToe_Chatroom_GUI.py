c748d22b5f23b8f854a28ec3e45e420159916f6c
NickHinchliffe <hinchliffen@wit.edu>
2019-12-03 22:31:45 -0500

    Moves and chats are being sent properly. Works with justin's introduction.
    *** NEED TO USE JUSTINS CODE TO ACCESS THE GUI ***
    
    Issues:
    1) Bug in winning the game
    2) Closing windows in Introduction

--- TicTacToe_Chatroom_GUI.py
+++ TicTacToe_Chatroom_GUI.py
@@ -5,6 +5,8 @@ from time import *
 
 #12/1/19 4PM
 bclick = True
+associatedName = ''
+
 
 def startGUI(profileName):
     tk = Tk()
@@ -20,8 +22,18 @@ def startGUI(profileName):
     tttSocket = socket(AF_INET, SOCK_STREAM)
     tttSocket.connect((serverName, portNumber))
 
-
-    def restart():
+    # Freezes all moves, sleeps for one second & resets board
+    def disable():
+        button1.configure(state=DISABLED)
+        button2.configure(state=DISABLED)
+        button3.configure(state=DISABLED)
+        button4.configure(state=DISABLED)
+        button5.configure(state=DISABLED)
+        button6.configure(state=DISABLED)
+        button7.configure(state=DISABLED)
+        button8.configure(state=DISABLED)
+        button9.configure(state=DISABLED)
+        sleep(1.5)
         button1['text'] = " "
         button2['text'] = " "
         button3['text'] = " "
@@ -40,20 +52,6 @@ def startGUI(profileName):
         button7.configure(state=NORMAL)
         button8.configure(state=NORMAL)
         button9.configure(state=NORMAL)
-
-
-
-    # Freezes all moves
-    def disable():
-        button1.configure(state=DISABLED)
-        button2.configure(state=DISABLED)
-        button3.configure(state=DISABLED)
-        button4.configure(state=DISABLED)
-        button5.configure(state=DISABLED)
-        button6.configure(state=DISABLED)
-        button7.configure(state=DISABLED)
-        button8.configure(state=DISABLED)
-        button9.configure(state=DISABLED)
 
     # Receives all incoming messages / handles specific messages
     def receive():
@@ -96,12 +94,12 @@ def startGUI(profileName):
                     button8['text'] = 'O'
                 if mes == '9,O':
                     button9['text'] = 'O'
-                if mes == 'Winner is X!\n':
+                if mes == associatedName + ' is the winner! \n':
                     disable()
-                if mes == 'Winner is O!\n':
-                    disable()
-                output.insert(INSERT, '%s\n' % mes)
-
+                if mes == 'Exit()':
+                    tk.quit()
+                else:
+                    output.insert(INSERT, '%s\n' % mes)
 
             except:
                 pass
@@ -112,9 +110,10 @@ def startGUI(profileName):
         sendMes = profileName + ": " + mes
         sendMesgo = sendMes.encode()
         if mes == '{quit}':
-            quitMessage = profileName+" has left the chat!\n"
+            quitMessage = profileName + " has left the chat!\n"
             tttSocket.send(quitMessage.encode())
             tttSocket.close()
+            tk.destroy()
         else:
             textentry.delete(0, END)
             tttSocket.send(sendMesgo)
@@ -130,6 +129,10 @@ def startGUI(profileName):
         winnerEnc = winner.encode()
         tttSocket.send(winnerEnc)
 
+    def addPoint(winner):
+        #call the function in introduction that I write
+        print("win win win")
+
     def sendOpponent(name):
         nameEnc = name.encode()
         tttSocket.send(nameEnc)
@@ -138,6 +141,7 @@ def startGUI(profileName):
     # Methods for when buttons are clicked
     def ttt(buttons):
         global bclick
+        global associatedName
         if buttons["text"] == "X":
             output.insert(INSERT, "Please choose an empty space for X\n")
         elif buttons["text"] == "O":
@@ -145,6 +149,7 @@ def startGUI(profileName):
 
         if buttons["text"] == " " and bclick == True:
             buttons["text"] = "X"
+            associatedName = profileName
             location = buttons.location
             strLocation = str(location)
             res = ''.join(filter(lambda i: i.isdigit(), strLocation))
@@ -154,6 +159,7 @@ def startGUI(profileName):
             bclick = False
         elif buttons["text"] == " " and bclick == False:
             buttons["text"] = "O"
+            associatedName = profileName
             location = buttons.location
             strLocation = str(location)
             res = ''.join(filter(lambda i: i.isdigit(), strLocation))
@@ -162,55 +168,72 @@ def startGUI(profileName):
             bclick = True
 
         # Checks to see if game has been won
+        rWinner = associatedName + ' is the winner! \n'
         xWinner = 'Winner is X!\n'
         oWinner = 'Winner is O!\n'
         if button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X':
-            sendWinner(xWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button4['text'] == 'X' and button5['text'] == 'X' and button6['text'] == 'X':
-            sendWinner(xWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button7['text'] == 'X' and button8['text'] == 'X' and button9['text'] == 'X':
-            sendWinner(xWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button1['text'] == 'X' and button5['text'] == 'X' and button9['text'] == 'X':
-            sendWinner(xWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button3['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X':
-            sendWinner(xWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X':
-            sendWinner(xWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button3['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X':
-            sendWinner(xWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
-        elif button2['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X':
-            sendWinner(xWinner)
+        elif button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X':
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         if button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O':
-            sendWinner(oWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O':
-            sendWinner(oWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O':
-            sendWinner(oWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button1['text'] == 'O' and button5['text'] == 'O' and button9['text'] == 'O':
-            sendWinner(oWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button3['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O':
-            sendWinner(oWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O':
-            sendWinner(oWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
         elif button3['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O':
-            sendWinner(oWinner)
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
-        elif button2['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O':
-            sendWinner(oWinner)
+        elif button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O':
+            sendWinner(rWinner)
+            addPoint(associatedName)
 
 
 
@@ -256,14 +279,14 @@ def startGUI(profileName):
 
     # Chatroom Creation & starts threads
     # Creates output box and set it in frame
-    output = Text(tk, width=30, wrap=WORD, background="white")
+    output = Text(tk, width=40, wrap=WORD, background="white")
     output.grid(row=1, column=3, rowspan=2, sticky=N + S + E + W)
     output.insert(INSERT, "Hello " + profileName + "! Welcome to TicTacToe!\n")
-    playerJoin = profileName + " has joined the game!"
+    playerJoin = profileName + " has joined the game!\n"
     sendOpponent(playerJoin)
 
     # Create input box and sets it in frame
-    textentry = Entry(tk, width=30, bg="white")
+    textentry = Entry(tk, width=40, bg="white")
     textentry.grid(row=3, column=3, stick=S + E + W)
 
     # Binds the enter key to the sendButton method
@@ -274,9 +297,6 @@ def startGUI(profileName):
     sendButton = Button(tk, text="Send", command=send)
     sendButton.grid(row=4, column=3, sticky=N)
 
-    # restart button
-    restartButton = Button(tk, text="Restart", command = restart)
-    restartButton.grid(row=4, column=4, sticky=N+W)
 
     # Creates thread to accept connections
     recThread = Thread(target=receive).start()
